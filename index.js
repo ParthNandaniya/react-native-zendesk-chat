287feebd139ca37b50cc9f03985852a077a37792
Parth Nandaniya <ParthNandaniya@github.com>
2020-06-15 13:40:16 +0530

    initialization

---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { ZendeskChat } = NativeModules;
+
+export default ZendeskChat;
---
+++ ios/ZendeskChat.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface ZendeskChat : NSObject <RCTBridgeModule>
+
+@end
